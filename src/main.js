f78671ec398b450fc5006884f408d7c38e44f118
Wildan Mubarok <willnode@wellosoft.net>
2018-02-05 20:34:51 +0700

    google analytics
    using ga-lite

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import app from './app.vue'
 import Vue from 'vue'
+import galite from 'ga-lite'
 import '../index.html'
 
 new Vue({
     el: '#app',
     render: h => h(app),
 })
+
+// Google analytics
+galite('create', 'UA-109180894-1', 'auto');
+galite('send', 'pageview');
