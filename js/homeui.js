83df9463b4a123a5b055a8e46eec8dbe7d05d97c
Wildan Mubarok <willnode@wellosoft.net>
2019-01-07 04:09:37 +0700

    Revisiting the home page 🔧

--- js/homeui.js
+++ js/homeui.js
@@ -112,3 +112,8 @@ if (graphics.bg)
 function track(str) {
     galite('send', 'event', 'Link', str);
 }
+
+particlesJS.load('particles-js', 'js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
