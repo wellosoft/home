7f8c40da10b9b3243c199aa7750676f6eec4cb79
Wildan M <willnode@wellosoft.net>
2020-02-08 16:46:39 +0700

    Rolling PWA

--- js/main.js
+++
@@ -1,8 +0,0 @@
-gotoEmail = function () {
-    $('.master-slick').slick('slickGoTo', 2);
-    $('#nav-check').prop('checked', false);
-}
-
-goTop = function () {
-    window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
-}
--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@
 })(window,document,"galite","script","//cdn.jsdelivr.net/npm/ga-lite@2/dist/ga-lite.min.js");
 galite('create', 'UA-109180894-1', 'auto');
 galite('send', 'pageview');
+
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', function() {
+		navigator.serviceWorker.register('/sw.js');
+	});
+}
